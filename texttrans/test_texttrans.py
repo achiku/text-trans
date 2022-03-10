ba06a18fc095360cdd8a6eafd3971e592e996847
Akira Chiku <akira.chiku@gmail.com>
2022-03-10 11:48:32 +0900

    Update

--- texttrans/test_texttrans.py
+++ texttrans/test_texttrans.py
@@ -1,5 +1,9 @@
 import pickle
+import math
+import tempfile
 import pytest
+
+from collections import defaultdict
 
 @pytest.mark.parametrize("word, prob", [
     ('pen', 0.11640052876679541),
@@ -11,14 +15,15 @@ def test_simple_char_transition_prob(word, prob):
     assert tp.prob(word) == prob
 
 
-@pytest.mark.parametrize("word, ngram", [
+@pytest.mark.parametrize("word, tpl", [
     ('apple', [('a', 'p'), ('p', 'p'), ('p', 'l'), ('l', 'e'), ('e', '\x00')]),
+    ('half', [('h', 'a'), ('a', 'l'), ('l', 'f'), ('f', '\x00')]),
 ])
-def test_sublines_for_ngram(word, ngram):
+def test_sublines_for_ngram(word, tpl):
     from .texttrans import TextTrans
     tp = TextTrans()
     t = [x for x in tp.sublines_for_ngram(word)]
-    assert t == ngram
+    assert t == tbl
 
 def test_load_model():
     """""
@@ -29,12 +34,49 @@ def test_load_model():
     """""
 
     d = pickle.load(open('../data/en.pki', 'rb'))
-    print(d)
+    print(d['mat']['a'])
+    assert False
 
 def test_train():
     """
     wordが1行に1つ入っているファイルを読み込み、遷移確率行列を作る
     """
     from .texttrans import TextTrans
-    tp = TextTrans('../examples/en_words.txt')
-    assert False
+    tp = TextTrans()
+    with tempfile.NamedTemporaryFile() as f:
+        tp.train('../examples/en_words.txt', f.name)
+        tran_prob_matrix = pickle.load(f)
+    assert tran_prob_matrix['mat']['a']['h'] == 0
+    assert tran_prob_matrix['mat']['a']['d'] == -3.1416861861770706
+
+@pytest.mark.parametrize("word, tpl", [
+    ('apple', [('a', 'p'), ('p', 'p'), ('p', 'l'), ('l', 'e'), ('e', '\x00')]),
+    ('half', [('h', 'a'), ('a', 'l'), ('l', 'f'), ('f', '\x00')]),
+])
+def test_bigram(word, tpl):
+    """
+    bigramを行う関数をよりシンプルに書けるか試す
+    (ngramだと変数等考えなければいけないがbigramならサクッと行けるのでは)
+    * https://albertauyeung.github.io/2018/06/03/generating-ngrams.html/
+    """
+    from .texttrans import bigram
+    r = [x for x in bigram(word)]
+    assert r == tpl
+
+
+def test_create_tran_prob_matrix():
+    """
+    遷移確率行列生成ロジックをよりシンプルに書けるか試す
+    ここのロジックをしっかり理解する
+    """
+    from .texttrans import create_matrix, TextTrans
+
+    prob_matrix_for_a = defaultdict()
+    with tempfile.NamedTemporaryFile() as f:
+        trainer = TextTrans()
+        trainer.train('../examples/en_words.txt', f.name)
+        tran_prob_matrix = pickle.load(f)
+
+    filepath = '../examples/en_words.txt'
+    res = create_matrix(filepath)
+    assert res['matrix']['a'] == tran_prob_matrix['mat']['a']
